b041975fdd206bd37fe35e1ccd667e6c0e67fe86
goodnews <goodnewsj62@gmail.com>
2022-07-18 10:25:11 +0100

    adding celery config and redis config

---
+++ osonwa/management/commands/wait_for_db.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from django.db import connections
+import time
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        self.stdout("Waiting for database....")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting for a second....")
+                time.sleep(1)
+
+            self.stdout.write(self.style.SUCCESS("DATABASE AVAILABLE"))
